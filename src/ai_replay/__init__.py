f002dcbd75c5c9cd617644636778833e3f9ab00c
Yann Debray <debray.yann@gmail.com>
2026-03-23 21:16:30 -0400

    Bump version to v0.1.1

--- src/ai_replay/__init__.py
+++ src/ai_replay/__init__.py
@@ -18,7 +18,7 @@ from .parser import parse_session, detect_format
 from .renderer import render_html
 from .resolve_session import resolve_session_path
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 VALID_THEMES = ["tokyo-night", "monokai", "solarized-dark", "github-light", "dracula", "bubbles"]
 
